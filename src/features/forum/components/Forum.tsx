82be99cf349239d2204c93c5877a07c957144427
pr0d1gy00 <calitoalejandro184@gmail.com>
2024-12-23 18:53:40 -0400

    se agrego buttonadd y modal en forum, falta maquetacion de modal

--- src/features/forum/components/Forum.tsx
+++ src/features/forum/components/Forum.tsx
@@ -2,9 +2,15 @@ import styles from '../styles/Forum.module.css'
 import Post from './Post'
 import PerfilIcon from '../../../assets/istockphoto-1495088043-612x612.webp'
 import FiltersPost from './FiltersPost'
+import { useGymStore } from '../../../store/store'
+import ButtonAdd from '../../../shared/buttons/components/ButtonAdd'
+import ModalAddRoutine from '../../routines/components/ModalAddRoutine'
 export default function Forum() {
+	const {modal}=useGymStore()
 	return (
-		<section className={styles.container}> 
+		<section className={styles.container}>
+			{!modal && <ButtonAdd/>}
+			{modal && <ModalAddRoutine/>} 
 			<h2>Comparte tus experiencias y ayuda a los demás en su vida <span>fitness</span></h2>
 			<FiltersPost/>
 			<section className={styles.postList}>
